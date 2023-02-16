7dfe685c0ed499f0eb10c4483e920556712d832b
Netiar <collimow54@gmail.com>
2023-02-16 10:59:10 +0300

    rf: move activity to ui directory

--- app/src/main/java/com/example/netia/ui/MainActivity.java
+++ app/src/main/java/com/example/netia/ui/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.netia;
+package com.example.netia.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.netia.R;
 
 public class MainActivity extends AppCompatActivity {
 
