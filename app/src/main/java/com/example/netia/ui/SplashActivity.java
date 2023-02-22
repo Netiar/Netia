1596357572b18859556afd75a48ba3c0c3ba6790
Netiar <collimow54@gmail.com>
2023-02-22 13:09:28 +0300

    add yoyo tada effect

--- app/src/main/java/com/example/netia/ui/SplashActivity.java
+++ app/src/main/java/com/example/netia/ui/SplashActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 import com.example.netia.R;
 
 public class SplashActivity extends AppCompatActivity {
@@ -12,5 +14,11 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        YoYo.with(Techniques.Tada)
+                .duration(2000)
+                .repeat(5)
+                .playOn(findViewById(R.id.textView));
+
     }
 }
